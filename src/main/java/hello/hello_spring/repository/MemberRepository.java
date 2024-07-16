0d6ee7f18c7c5cc7ba551fcc7fa09580b3ff1aa0
minoooo119 <minoth0119@naver.com>
2024-07-16 17:25:18 +0900

    [add] 비지니스 요구 사항 실습:리포지토리 인터페이스

---
+++ src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+
+    //null 이 있는 경우 처리해주기 위해서 Optional 로 한다.
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
