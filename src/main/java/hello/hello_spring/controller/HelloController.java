5580ccf4211c0f2e3e585385a670ef8b51ea8458
minoooo119 <minoth0119@naver.com>
2024-07-16 16:48:47 +0900

    객체로 전달하기

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,7 +3,9 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 //컨트롤러는 애초에 View를 반환하기 위해서 사용된다.
 //client 요청에 따라 View를 반환하게 된다.
@@ -28,5 +30,32 @@ public class HelloController {
         //url로 인자를 넘겨서 name에 값을 넣어주게 된다.
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+    //html로 내리느냐, api로 데이터를 내리느냐가 있다.
+    @GetMapping("hello-string")
+    @ResponseBody //이런 방식은 큰 의미는 없긴함.
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api") //객체를 받고 ResponseBody로 반환하면 Json 방식으로 반환한다.
+    @ResponseBody // 이 어노테이션을 활용하면 http의 body에 문자 내용을 직접 반환한다. view resolver 대신에 http message converter가 동작한다.
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+        //json 방식으로 리턴을 해주게 된다.
+    }
+    static class Hello{
+        private String name;
+        // 자바 빈 표준 방식 등 언어가 있음
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
