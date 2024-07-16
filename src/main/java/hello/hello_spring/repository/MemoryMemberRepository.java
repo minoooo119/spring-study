b9e765c7bdcacd4fd5b8836e7172a7b2bedabc2d
minoooo119 <minoth0119@naver.com>
2024-07-16 17:25:32 +0900

    [add] 비지니스 요구 사항 실습:메모리 리포지토리

---
+++ src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,43 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+
+import java.util.*;
+
+// 잘 동작하는지 확인하려면 보통 테스트 케이스 만들어서 한다.
+
+public class MemoryMemberRepository implements MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(),member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+//        return store.get(id);
+//        null 일 수도 있으므로 위처럼 안한다.
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
