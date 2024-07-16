ca65428df266cdbbcdc797798a53df06eb875fc9
minoooo119 <minoth0119@naver.com>
2024-07-16 15:38:55 +0900

    [add] controller, html 컨트롤러와 그에 해당하는 정적 html 추가

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -1,6 +1,5 @@
 package hello.hello_spring.controller;
 
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
