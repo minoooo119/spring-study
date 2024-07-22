b6c944e3b3b356ffb42fcd6bcc6d5f9b24f02f35
minoooo119 <minoth0119@naver.com>
2024-07-22 16:11:00 +0900

    [add] for user login

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -14,6 +14,8 @@ public class SpringConfig {
 
 //    private EntityManager em;
     private final MemberRepository memberRepository;
+    //spring data jpa 가 알아서 구현체를 만들어 주기에 위에 처럼 그냥 가져다가 쓰면 된다!
+    //아래처럼 인젝션 받으면 된다.
 
     //생성자가 하나인 경우에느 Autowired 생략가능
     @Autowired
@@ -42,3 +44,37 @@ public class SpringConfig {
 //    }
 
 }
+
+//아래 경우는 JPA 만 사용하는 경우의 예시이다.
+
+//package hello.hellospring;
+//import hello.hellospring.repository.*;
+//import hello.hellospring.service.MemberService;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import javax.persistence.EntityManager;
+//import javax.sql.DataSource;
+
+//@Configuration
+//public class SpringConfig {
+//    private final DataSource dataSource;
+//    private final EntityManager em;
+//
+//    public SpringConfig(DataSource dataSource, EntityManager em) {
+//        this.dataSource = dataSource;
+//        this.em = em;
+//    }
+//
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
+//}
--- src/main/java/hello/hello_spring/repository/MemberRepository.java
+++ src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 
 public interface MemberRepository {
     Member save(Member member);
-
     //null 이 있는 경우 처리해주기 위해서 Optional 로 한다.
     Optional<Member> findById(Long id);
     Optional<Member> findByName(String name);
